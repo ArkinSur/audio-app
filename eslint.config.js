9e6274d7f46a31a55353da7797b3b6b0103da588
Filipe Drummond <exgavian82@gmail.com>
2025-08-25 13:56:19 -0300

    chore: initial commit

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,52 @@
 // https://docs.expo.dev/guides/using-eslint/
 const { defineConfig } = require('eslint/config');
 const expoConfig = require('eslint-config-expo/flat');
+const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
 
 module.exports = defineConfig([
-  expoConfig,
+  ...expoConfig,
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+
+    settings: {
+      'import/resolver': {
+        typescript: {
+          alwaysTryTypes: true,
+        },
+      },
+    },
+
+    rules: {
+      'import/order': [
+        'error',
+        {
+          groups: [
+            'builtin',
+            'external',
+            'internal',
+            ['parent', 'sibling', 'index'],
+            'object',
+            'type',
+          ],
+          pathGroups: [
+            { pattern: 'react', group: 'external', position: 'before' },
+            { pattern: 'react-native', group: 'external', position: 'after' },
+            { pattern: '@/**', group: 'internal' },
+          ],
+          pathGroupsExcludedImportTypes: ['react'],
+          alphabetize: { order: 'asc', caseInsensitive: true },
+          'newlines-between': 'always',
+        },
+      ],
+
+      'import/no-unresolved': 'error',
+
+      'import/first': 'error',
+      'import/newline-after-import': 'error',
+      'import/no-duplicates': 'error',
+    },
+  },
+  eslintPluginPrettierRecommended,
   {
     ignores: ['dist/*'],
   },
