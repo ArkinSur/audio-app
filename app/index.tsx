9c0664f07d8285152443f1371d9d7a9f4a0775d3
Filipe Drummond <exgavian82@gmail.com>
2025-08-25 19:30:50 -0300

    feat: initial audio controls

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,84 @@
-import { StyleSheet, View } from 'react-native';
+import { useCallback } from 'react';
+
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { useAudioPlayer, useAudioPlayerStatus } from 'expo-audio';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+
+import { StyleSheet, TouchableOpacity, View } from 'react-native';
+
+const audioSource = require('../assets/audio/example_audio.mp3');
 
 export default function HomeScreen() {
-  return <View style={styles.container} />;
+  const player = useAudioPlayer(audioSource);
+  const status = useAudioPlayerStatus(player);
+
+  const insets = useSafeAreaInsets();
+
+  const onPressPlay = useCallback(() => {
+    if (player.paused) {
+      player.play();
+    } else {
+      player.pause();
+    }
+  }, [player]);
+
+  const onPressBack = useCallback(() => {
+    player.pause();
+    player.seekTo(0);
+  }, [player]);
+
+  return (
+    <View
+      style={[
+        styles.container,
+        { paddingTop: insets.top, paddingBottom: insets.bottom },
+      ]}
+    >
+      <View
+        style={{ flex: 1, backgroundColor: '#ededed', paddingHorizontal: 16 }}
+      />
+      <View style={styles.footer}>
+        <TouchableOpacity
+          onPress={onPressBack}
+          style={[styles.playButton, { backgroundColor: '#fff' }]}
+        >
+          <Ionicons name="play-back" size={28} color="black" />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={onPressPlay} style={styles.playButton}>
+          {!status.playing ? (
+            <Ionicons name="play" size={28} color="black" />
+          ) : (
+            <Ionicons name="pause" size={28} color="black" />
+          )}
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={[styles.playButton, { backgroundColor: '#fff' }]}
+        >
+          <Ionicons name="play-forward" size={28} color="black" />
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  footer: {
+    height: 120,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    columnGap: 16,
+  },
+  playButton: {
+    height: 54,
+    width: 54,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 27,
+    backgroundColor: '#ffd91a',
+  },
 });
